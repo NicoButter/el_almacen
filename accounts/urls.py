45ee09b266b7f9a8488452961f8ec03704d89241
Nicolás Butterfield <nicobutter@gmail.com>
2024-10-14 16:22:33 -0300

    login updates

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register, user_login
+
+urlpatterns = [
+    path('login/', user_login, name='login'), 
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),  
+    path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),  
+    path('cashier_dashboard/', cashier_dashboard, name='cashier_dashboard'),  
+    path('user_dashboard/', user_dashboard, name='user_dashboard'), 
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+# views.py en la aplicación accounts
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def register(request):
+    if not request.user.is_staff:  
+        return redirect('login')  
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirigir al inicio de sesión después del registro
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirigir al dashboard correspondiente
+                if user.is_admin:
+                    return redirect('admin_dashboard')  # Cambia esto según la URL de tu dashboard
+                elif user.is_cashier:
+                    return redirect('cashier_dashboard')
+                else:
+                    return redirect('user_dashboard')  # Cambia según sea necesario
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
