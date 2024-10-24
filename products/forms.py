2839db05f45927891507329cfdd0d2751cd36878
Nicolás Butterfield <nicobutter@gmail.com>
2024-10-24 14:30:05 -0300

    several updates on styles

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,21 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    SE_VENDE_FRACCIONADO_CHOICES = [
+        (True, 'Sí'),
+        (False, 'No'),
+    ]
+
+    # se_vende_fraccionado = forms.ChoiceField(
+    #     choices=SE_VENDE_FRACCIONADO_CHOICES,
+    #     widget=forms.Select(attrs={'class': 'form-control'})
+    # )
+
+    se_vende_fraccionado = forms.BooleanField(
+        required=False, 
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
     class Meta:
         model = Product
-        fields = ['nombre', 'descripcion', 'precio', 'cantidad_stock', 'imagen', 'se_vende_fraccionado'] 
-
+        fields = ['nombre', 'descripcion', 'precio', 'cantidad_stock', 'imagen', 'se_vende_fraccionado']
